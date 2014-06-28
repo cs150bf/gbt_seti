a70ba5e10f11787d56438980ff218792dc98437b
hchen <chen.hong.1992@gmail.com>
2014-06-27 18:49:32 -0700

    trivial update

--- path_setup.sh
+++ path_setup.sh
@@ -6,8 +6,8 @@ export CUDA_PATH=/usr/local/cuda
 export PATH=${PATH//":${CUDA_PATH}/bin"/}
 export PATH=${PATH}:${CUDA_PATH}/bin
 export PRESTO=/home/siemion/sw/presto
-echo $SETI_GBT
-echo $LD_LIBRARY_PATH
-echo $CUDA_PATH
-echo $PRESTO
-echo $PATH
+echo "SETI_GBT="$SETI_GBT
+echo "LD_LIBRARY_PATH="$LD_LIBRARY_PATH
+echo "CUDA_PATH="$CUDA_PATH
+echo "PRESTO="$PRESTO
+echo "PATH="$PATH
