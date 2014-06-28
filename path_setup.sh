294ebaec781381cfec7dc40b47bbd9440a028f83
hchen <chen.hong.1992@gmail.com>
2014-06-27 18:45:00 -0700

    some random setup thing for lazy ones (do source path_setup.sh before make)

---
+++ path_setup.sh
@@ -0,0 +1,13 @@
+export SETI_GBT=${PWD%'/src'}
+export SETI_GBT=${SETI_GBT%'/bin'}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH//":${SETI_GBT}/lib"/}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SETI_GBT}/lib
+export CUDA_PATH=/usr/local/cuda
+export PATH=${PATH//":${CUDA_PATH}/bin"/}
+export PATH=${PATH}:${CUDA_PATH}/bin
+export PRESTO=/home/siemion/sw/presto
+echo $SETI_GBT
+echo $LD_LIBRARY_PATH
+echo $CUDA_PATH
+echo $PRESTO
+echo $PATH
